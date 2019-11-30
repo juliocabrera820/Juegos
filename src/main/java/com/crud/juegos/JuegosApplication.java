797f7f51429de9289c78dab697ece71f01699cba
juliocabrera820 <juliocabrera820@gmail.com>
2019-11-30 16:29:19 -0600

    Aplicacion

---
+++ src/main/java/com/crud/juegos/JuegosApplication.java
@@ -0,0 +1,13 @@
+package com.crud.juegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuegosApplication.class, args);
+    }
+
+}
